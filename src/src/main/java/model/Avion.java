be973801e3e37564f2dce8d82d00de2803460eea
IxSysTech <simeonovic2904@gmail.com>
2019-11-11 09:48:39 +0100

    Update du fichier .gitignore

--- src/src/main/java/model/Avion.java
+++ src/src/main/java/model/Avion.java
@@ -8,18 +8,38 @@ public class Avion {
     private final String compagnie;
     private final String type;
 
+    /**
+     * Constructeur
+     * @param id Id de l'avion
+     * @param compagnie Compagnie de l'avion
+     * @param type Type de l'avion
+     */
     public Avion(int id, String compagnie, String type) {
         this.id = id;
         this.compagnie = compagnie;
         this.type = type;
     }
 
+    /**
+     * Permet de récupérer la compagnie de l'avion
+     * @return
+     */
     public String getCompagnie() {
         return compagnie;
     }
+
+    /**
+     * Permet de récupérer le type de l'avion
+     * @return
+     */
     public String getType() {
         return type;
     }
+
+    /**
+     * Permet de récupérer l'id de l'avion
+     * @return
+     */
     public int getId() {
         return id;
     }
